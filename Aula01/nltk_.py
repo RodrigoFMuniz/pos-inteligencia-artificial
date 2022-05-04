5c91b0b33c7b29d113351286f1b4a93ae16171fe
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-03 23:15:34 -0300

    Aula 01, usando nltk

---
+++ Aula01/nltk_.py
@@ -0,0 +1,8 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer, SnowballStemmer, LancasterStemmer
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tag import pos_tag, pos_tag_sents
+import string
+nltk.download()
+
