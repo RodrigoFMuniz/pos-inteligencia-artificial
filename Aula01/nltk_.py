987d6ea56dca5a630600494e5be6dc4ca027ac76
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-05 17:01:37 -0300

    Aula 1, NLTK, stemming, Porter

--- Aula01/nltk_.py
+++ Aula01/nltk_.py
@@ -12,7 +12,7 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
-texto = "Nós somos feitos de poeiras de estrelas. Nós somos uma maneira de os cosmos se auto conhecer. A imaginação nos leva a mundos que sequer existiram, mas sem ela não vamos a lugar algum."
+texto = "Nós somos feitos de poeiras de estrelas. Nós somos uma maneira de os cosmos se auto conhecer. A imaginação nos leva a mundos que sequer existiram, mas sem ela não vamos a lugar algum. Maneiro são as maneiras de maneirismos, imagine se assim não fossem"
 
 sentencas = sent_tokenize(texto, language="portuguese")
 
@@ -20,7 +20,7 @@ print(sentencas)
 print(type(sentencas))
 print(len(sentencas))
 
-words = word_tokenize(texto,language="portuguese")
+words = word_tokenize(texto, language="portuguese")
 
 print(words)
 print(type(words))
@@ -36,6 +36,20 @@ print(words_wo_stopwords)
 
 print(string.punctuation)
 
-texto_wo_stopwords_and_punctuation = [w for w in words_wo_stopwords if w not in string.punctuation]
+texto_wo_stopwords_and_punctuation = [
+    w for w in words_wo_stopwords if w not in string.punctuation]
 
-print (texto_wo_stopwords_and_punctuation)
+print('---------------------------------')
+print('Sem pontuação e sem stopwords')
+print(texto_wo_stopwords_and_punctuation)
+
+freq = nltk.FreqDist(texto_wo_stopwords_and_punctuation)
+print(freq.most_common(5))
+freq_words = freq.pformat
+
+
+print('---------------------------------')
+print('Stem PorterSteamer')
+steamer = PorterStemmer()
+stem1 = [steamer.stem(words) for words in texto_wo_stopwords_and_punctuation]
+print(stem1)
