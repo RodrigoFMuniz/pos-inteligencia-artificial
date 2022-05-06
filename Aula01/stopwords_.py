18aa9b821261fec780be2a93dae715698b10e86a
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-06 00:30:14 -0300

    Aula 01, punctuation_.py

--- Aula01/stopwords_.py
+++ Aula01/stopwords_.py
@@ -1,13 +1,14 @@
 from base_nltk import *
 from tokenize_ import words
 
-print("------------------------------------")
-print("Stopwords")
-
 stops = stopwords.words("portuguese")
-print(len(stops))
-# print(stops)
-
 words_wo_stopwords = [w for w in words if w not in stops]
 
-print(words_wo_stopwords)
+if __name__ == "__main__":
+    print("------------------------------------")
+    print("Stopwords")
+
+    print(len(stops))
+    # print(stops)
+
+    print(words_wo_stopwords)
