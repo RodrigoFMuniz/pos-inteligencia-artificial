ab82748da2f462e551344833db6c869126ce7d52
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-05 23:44:49 -0300

    Aula 01, Tokenize file

--- Aula01/tokenize_.py
+++ Aula01/tokenize_.py
@@ -11,16 +11,19 @@ nltk.download('words')
 
 texto = "Nós somos feitos de poeiras de estrelas. Nós somos uma maneira de os cosmos se auto conhecer. A imaginação nos leva a mundos que sequer existiram, mas sem ela não vamos a lugar algum. Maneiro são as maneiras de maneirismos, imagine se assim não fossem"
 
-sentencas = sent_tokenize(texto, language="portuguese")
 
 print("------------------------------------")
 print("Sentenças Tokenize")
+
+sentencas = sent_tokenize(texto, language="portuguese")
+
 print(sentencas)
 print(type(sentencas))
 print(len(sentencas))
 
 print("------------------------------------")
 print("Words Tokenize")
+
 words = word_tokenize(texto, language="portuguese")
 
 print(words)
