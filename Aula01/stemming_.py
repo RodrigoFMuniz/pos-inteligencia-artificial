d97839fb52a9d6ca7ab9cfea0ada94a054388865
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-06 00:45:43 -0300

    Aula 01, stemming.py

--- Aula01/stemming_.py
+++ Aula01/stemming_.py
@@ -1,10 +1,28 @@
-import nltk
-from nltk.stem import PorterStemmer, WordNetLemmatizer, SnowballStemmer, LancasterStemmer
-import string
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('tagsets')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
+from base_nltk import *
+from punctuation_ import texto_wo_stopwords_and_punctuation
+
+
+if __name__ == "__main__":
+    print('---------------------------------')
+    print(texto)
+
+    print('---------------------------------')
+    print('Stem PorterSteamer')
+    steamer = PorterStemmer()
+    stem1 = [steamer.stem(words)
+             for words in texto_wo_stopwords_and_punctuation]
+    print(stem1)
+
+    print('---------------------------------')
+    print('Stem SnowballSteamer')
+    steamer2 = SnowballStemmer('portuguese')
+    stem2 = [steamer2.stem(words)
+             for words in texto_wo_stopwords_and_punctuation]
+    print(stem2)
+
+    print('---------------------------------')
+    print('Stem LancasterSteamer')
+    steamer3 = LancasterStemmer()
+    stem3 = [steamer3.stem(words)
+             for words in texto_wo_stopwords_and_punctuation]
+    print(stem3)
