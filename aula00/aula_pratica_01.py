e4f5f90f51f8b3e4caf54f6ee7ae147e0a3d24f0
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-05-07 23:22:12 -0300

    utilizando dtale

--- aula00/aula_pratica_01.py
+++ aula00/aula_pratica_01.py
@@ -1,7 +1,11 @@
 import nltk
 import dtale
+import dtale.app as dtale_app
 import wordcloud
 import pandas as pd
 
+dtale_app.USE_COLAB = False
+
 data = pd.read_excel('../assets/Vagas_para_Cientista_e_Analista_de_Dados.xlsx')
-print(data)
+dtale.show(data)
+
